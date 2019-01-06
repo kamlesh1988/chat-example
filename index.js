53bde7404f2b25985ef899eb2b7b274fad61c33b
kamlesh patel <kamlesh.patel.djs@gmail.com>
2019-01-06 10:23:07 +0000

    updated socket.io example

--- index.js
+++ index.js
@@ -3,16 +3,24 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
+io.on('connection', function (socket) {
+    console.log('a user connected');
+    socket.on('chat message', function (msg) {
+        console.log('Message received ', msg);
+        io.emit('chat message', msg);
+    });
+    socket.on('event', function (e) {
+        console.log('event : ', e);
+    });
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+    });
 });
 
-http.listen(port, function(){
-  console.log('listening on *:' + port);
+http.listen(port, function () {
+    console.log('listening on *:' + port);
 });
